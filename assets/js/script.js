2412300a3d0e945476dce50ac1cfc4e00e0c42dc
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2022-12-13 19:27:54 -0500

    search history working, settin up evnt list

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,42 @@ var windEls = document.querySelectorAll('.windEl');
 var searchBtn = document.querySelector('#searchBtn');
 var cityEl = document.querySelector('#cityEl');
 // var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=9379b40dea2a6d930a04b31663b3083f'
+var prevSearches = JSON.parse(localStorage.getItem('cities'));
+var displayHistory = function () {
+  if (prevSearches != null) {
+    for (var search of prevSearches) {
+      var histBtn = document.createElement('button');
+      histBtn.textContent = search;
+      histBtn.setAttribute('class', 'searchHistory');
+      document.getElementById('btnsDiv').appendChild(histBtn);
+    }
+  }
+}
 
-// 4 12 20 28 36
+displayHistory();
+
+var btnsDiv = document.getElementById('btnsDiv');
+btnsDiv.addEventListener('click', function (event) {
+  var clicked = event.target;
+
+})
+
 searchBtn.addEventListener('click', function () {
   var city = document.querySelector('input').value
   var coRequestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=9379b40dea2a6d930a04b31663b3083f';
 
-  var prevSearches = JSON.parse(localStorage.getItem('cities'));
   var saveSearch = function (userInput) {
     if (prevSearches !== null) {
       prevSearches.push(userInput);
       localStorage.setItem('cities', JSON.stringify(prevSearches));
+      displayHistory();
     } else {
       prevSearches = [userInput];
       localStorage.setItem('cities', JSON.stringify(prevSearches));
+      displayHistory();
     }
   }
+  saveSearch(city);
 
 
   fetch(coRequestUrl)
@@ -41,7 +61,7 @@ searchBtn.addEventListener('click', function () {
         .then(function (data) {
           console.log(data);
           cityEl.textContent = 'Upcoming Weather in ' + data.city.name;
-          for (i = 4; i < 37; i += 8) {
+          for (var i = 4; i < 37; i += 8) {
             document.getElementById(`temp${i}`).textContent = 'Temp: ' + data.list[i].main.temp;;
             document.getElementById(`date${i}`).textContent = data.list[i].dt_txt;
             document.getElementById(`humid${i}`).textContent = 'Humidity: ' + data.list[i].main.humidity;
